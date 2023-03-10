e5cde08376dde698215997766907da121bd64ada
yosuanovry <yosuanovry@gmail.com>
2023-03-10 18:20:02 +0700

    fix

--- src/middleware/uploadPhoto.js
+++ src/middleware/uploadPhoto.js
@@ -1,9 +1,6 @@
 const multer = require('multer')
 
 const storage = multer.diskStorage({
-    destination: function(req,file,cb){
-        cb(null,'./tmp')
-    },
     filename: function(req,file,cb){
         const uniq = Date.now() + Math.round(Math.random() * 1E9)
         cb(null,uniq+'.png')
