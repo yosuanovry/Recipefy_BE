a8321591a09a6041f85403b615c8b24b595ddde0
yosuanovry <yosuanovry@gmail.com>
2023-03-11 22:24:06 +0700

    fix

--- src/routes/recipeRoute.js
+++ src/routes/recipeRoute.js
@@ -6,8 +6,8 @@ const upload = require("../middleware/uploadPhoto");
 const validateFile = require("../middleware/validatePhoto");
 
 router.post("/", protect, upload.single("photo"), validateFile, inputRecipes);
-router.get("/", getRecipesByName);
 router.get("/recipes/:id", selectDataById);
+router.get("/", getRecipesByName);
 router.get("/my-recipes", protect, getRecipesById);
 router.delete("/:id", protect, deleteRecipes);
 router.put("/:id", protect, upload.single("photo"), validateFile, updateRecipeData);
