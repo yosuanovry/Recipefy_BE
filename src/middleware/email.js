97a1a943c93c67c3905952d186cdc89511bf82ca
yosuanovry <yosuanovry@gmail.com>
2023-04-13 15:31:36 +0700

    try fix nodemailer

--- src/middleware/email.js
+++ src/middleware/email.js
@@ -1,32 +1,31 @@
-const nodemailer = require("nodemailer")
-
+const nodemailer = require("nodemailer");
 
 let transporter = nodemailer.createTransport({
-    host: "smtp.zoho.com",
-    secure: true,
-    port: 465,
-    auth: {
-      user: process.env.EMAIL_NAME,
-      pass: process.env.EMAIL_PASSWORD
-    },
-  });
-  
-  module.exports = (emailClient,subject,url,name) => {
-    let mailOption ={
-        from:  process.env.EMAIL_NAME,
-        to: emailClient,
-        subject:`${subject} is your otp`,
-        text: `Hello ${name}, ${subject} is your otp, please input in form ${url}`
-    }
+  host: "smtp.zoho.com",
+  secure: true,
+  port: 465,
+  auth: {
+    user: process.env.EMAIL_NAME,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
 
-    transporter.sendMail(mailOption,function(error,data){
-        if(error){
-            console.log("error : ", error)
-            return "email not send"
-        } else{
-            console.log("email send")
-            console.log(data)
-            return "email success"
-        }
-    })
-  }
+module.exports = (emailClient, subject, url, name) => {
+  let mailOption = {
+    from: process.env.EMAIL_NAME,
+    to: emailClient,
+    subject: `${subject} is your otp`,
+    text: `Hello ${name}, ${subject} is your otp, please input in form ${url}`,
+  };
+  new Promise((resolve, reject) => {
+    transporter.sendMail(mailOption, function (error, data) {
+      if (error) {
+        console.log("error : ", error);
+        reject(error, "email not send");
+      } else {
+        resolve("email send");
+        console.log(data);
+      }
+    });
+  });
+};
